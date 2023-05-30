7d79ba7927d4aeeca722f905f7dedf19316b77e6
tot <126389606+totkyle@users.noreply.github.com>
2023-05-30 22:10:16 +0300

    Add files via upload

---
+++ start.js
@@ -0,0 +1,3 @@
+import{DEV}from '/esm-env.js?v=c4d5c7f4';import{create_client}from '/client.js?v=c4d5c7f4';import{init}from '/node_modules/@sveltejs/kit/src/runtime/client/singletons.js?v=c4d5c7f4';export async function start(app,target,hydrate){if(DEV&&target===document.body){console.warn(`Placing %sveltekit.body% directly inside <body> is not recommended, as your app may break for users who have certain browser extensions installed.\n\nConsider wrapping it in an element:\n\n<div style="display: contents">\n  %sveltekit.body%\n</div>`);}
+const client=create_client(app,target);init({client});if(hydrate){await client._hydrate(hydrate);}else{client.goto(location.href,{replaceState:true});}
+client._start_router();}
